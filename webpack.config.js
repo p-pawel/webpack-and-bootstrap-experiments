71693e5eb1597eeff83847d23bedfd409e1c92b5
p-pawel <git@ppawel.net>
2016-06-16 12:03:42 +0200

    At least it works...

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+    context: __dirname + "/app",
+    entry: [
+        './index.js'
+    ],
+    output: {
+        path: __dirname + '/app',
+        filename: 'bundle.js'
+    },
+    module: {
+        // ### Loaders
+        loaders: [
+            // **IMPORTANT** This is needed so that each bootstrap js file required by
+            // bootstrap-webpack has access to the jQuery object
+            { test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery' },
+
+            // Needed for the css-loader when [bootstrap-webpack](https://github.com/bline/bootstrap-webpack)
+            // loads bootstrap's css.
+            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&minetype=application/font-woff" },
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&minetype=application/font-woff2" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=application/octet-stream" },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=image/svg+xml" }
+        ]
+    }
+};
